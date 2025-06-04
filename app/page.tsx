54be4c2956851e33c9e6dfa81e1ffae4e5ba4199
Logan Szeto <loganszeto@gmail.com>
2025-06-04 11:11:37 -0700

    Changed a few things up, like centering and colors

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
   useEffect(() => {
     // Calculate delays based on text length and typing speed (50ms per character)
     const line1Text = 'Irvine, CA';
-    const line2Text = 'Student at UCI studying software engineering with a';
-    const line3Text = 'focus in data and AI alignment. Strength Training Enthusiast.';
+    const line2Text = 'Student at UCI studying software engineering with a focus';
+    const line3Text = 'in data and AI alignment. Strength Training Enthusiast.';
     
     const line1Time = line1Text.length * 50;
     const line2Time = line2Text.length * 50;
@@ -46,58 +46,64 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col min-h-screen pl-40 pt-10">
-      <h1 className="text-[#e6c384] text-5xl mb-6">Logan Szeto</h1>
+    <div className="flex flex-col min-h-screen pl-26 pt-10">
+      <div className="h-[300px]"> {/* Fixed height container for intro content */}
+        <h1 className="text-[#e6c384] text-5xl mb-6">Logan Szeto</h1>
 
-      {/* Line 1 */}
-      <div className="text-[#947fb8] text-lg mb-1">
-        <Typewriter
-          words={['Irvine, CA']}
-          typeSpeed={50}
-          deleteSpeed={0}
-          delaySpeed={0}
-          loop={1}
-          cursor={cursorLine1}
-          cursorStyle="_"
-        />
-      </div>
-
-      {/* Line 2 */}
-      {showLine2 && (
+        {/* Line 1 */}
         <div className="text-gray-400 text-lg mb-1">
           <Typewriter
-            words={['Student at UCI studying software engineering with a']}
+            words={['Irvine, CA']}
             typeSpeed={50}
             deleteSpeed={0}
             delaySpeed={0}
             loop={1}
-            cursor={cursorLine2}
+            cursor={cursorLine1}
             cursorStyle="_"
           />
         </div>
-      )}
 
-      {/* Line 3 */}
-      {showLine3 && (
-        <div className="text-gray-400 text-lg mb-4">
-          <Typewriter
-            words={['focus in data and AI alignment. Strength Training Enthusiast.']}
-            typeSpeed={50}
-            deleteSpeed={0}
-            delaySpeed={0}
-            loop={1}
-            cursor={cursorLine3}
-            cursorStyle="_"
-          />
-        </div>
-      )}
-      {/* Posts + Data */}
-      <h1 className="text-[#e6c384] mt-10 text-3xl mb-6">posts</h1>
-      <div className="text-gray-400 text-lg mb-4">. . .</div>
-      <div className="text-gray-400 text-lg mb-4">. . .</div>
-      <h1 className="text-[#e6c384] mt-2 text-3xl mb-6">data</h1>
-      <div className="text-gray-400 text-lg mb-4">. . .</div>
-      <div className="text-gray-400 text-lg mb-4">. . .</div>
+        {/* Line 2 */}
+        {showLine2 && (
+          <div className="text-gray-400 text-lg mb-1">
+            <Typewriter
+              words={['Student at UCI studying software engineering with a']}
+              typeSpeed={50}
+              deleteSpeed={0}
+              delaySpeed={0}
+              loop={1}
+              cursor={cursorLine2}
+              cursorStyle="_"
+            />
+          </div>
+        )}
+
+        {/* Line 3 */}
+        {showLine3 && (
+          <div className="text-gray-400 text-lg mb-4">
+            <Typewriter
+              words={['focus in data and AI alignment. Strength Training Enthusiast.']}
+              typeSpeed={50}
+              deleteSpeed={0}
+              delaySpeed={0}
+              loop={1}
+              cursor={cursorLine3}
+              cursorStyle="_"
+            />
+          </div>
+        )}
+
+      </div>
+
+      {/* Posts + Data Section */}
+      <div className="mt-10">
+        <h1 className="text-[#e6c384] text-3xl mb-6">posts</h1>
+        <div className="text-gray-400 text-lg mb-4">. . .</div>
+        <div className="text-gray-400 text-lg mb-4">. . .</div>
+        <h1 className="text-[#e6c384] mt-2 text-3xl mb-6">data</h1>
+        <div className="text-gray-400 text-lg mb-4">. . .</div>
+        <div className="text-gray-400 text-lg mb-4">. . .</div>
+      </div>
     </div>
   );
 }
